0bec0cbe44cccf8288a27c98c8f9e5323f3d2c63
Tam Nguyen <nguyenminhtam7124@gmail.com>
2023-11-11 06:59:36 -0500

    Create a playground route

---
+++ src/app/playground/page.tsx
@@ -0,0 +1,4 @@
+//  This file is for the Playground page.
+//  It enable user to code Py-JSON through clicking buttons.
+//  There will be several buttons for user to click, including:
+// Print, Input, If, For, While, Function, Class, and Comment.
